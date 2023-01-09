822aac35e83313bfb87a076e31e9cec0434b4d37
jan.simek <jan.simek@konicaminolta.cz>
2023-01-09 12:13:27 +0100

    Styling code

--- src/components/FiveDays.vue
+++ src/components/FiveDays.vue
@@ -1,13 +1,12 @@
 <template>
     <div class=" mx-1 flex flex-col gap-y-1 items-stretch bg-slate-500">
         <div class="flex flex-col gap-y-2 sticky top-0 bg-slate-500">
-            <div class="flex flex-row justify-between font-bold  text-4xl text-red-600
-                    p-4 rounded-md bg-slate-600">
+            <div class="flex flex-row justify-between font-bold  text-4xl text-red-600 p-4 rounded-md bg-slate-600">
                 <svg 
-                    @click="toggleParams()" 
                     class="cursor-pointer self-center" 
                     style="width:50px;height:50px" 
                     viewBox="0 0 30 30"
+                    @click="toggleParams()" 
                 >
                     <path fill="red" d="M15,5H17V3H15M15,13H17V11H15M15,21H17V19H15M11,5H13V3H11M19,5H21V3H19M11,9H13V7H11M19,9H21V7H19M19,21H21V19H19M19,13H21V11H19M19,17H21V15H19M11,13H13V11H11M3,5H5V3H3M3,9H5V7H3M3,13H5V11H3M3,17H5V15H3M3,21H5V19H3M11,21H13V19H11M11,17H13V15H11M7,21H9V19H7M7,13H9V11H7M7,5H9V3H7V5Z" />
                 </svg>
@@ -22,8 +21,8 @@
             ">
                 <svg 
                     v-for="(param, index) in params" :key="index"
-                    @click="param.show = !param.show" 
                     class="cursor-pointer" style="width:50px;height:50px" viewBox="0 0 30 30"
+                    @click="param.show = !param.show" 
                 >
                     <path :fill="param.show ? 'deepskyblue' : 'gray'" :d="param.svg" />
                 </svg>
@@ -54,7 +53,6 @@
                     <img 
                         :src="getIconUrl(index)"
                         width="70"
-                        class=""
                     >
 
                     <div class="flex flex-row justify-center gap-x-3">
